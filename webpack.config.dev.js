f1194aef3ee4e826a1c66560327a832e3ac44cb2
Septian A. Fujianto <septianfujianto@gmail.com>
2022-06-19 22:22:10 +0700

    add clean-webpack-plugin to clean each build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,20 @@
+const path = require('path')
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
 });
 
+const cleanWebpackPlugin = new CleanWebpackPlugin()
+
 module.exports = {
   mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [{
       test: /\.js$/,
@@ -20,5 +29,5 @@ module.exports = {
     }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlPlugin, cleanWebpackPlugin]
 };
